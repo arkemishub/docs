7c98aeef36cbdba7275592e696d1da9789ebfa0b
Manolo Battista <manolo@arkemis.it>
2022-04-15 15:12:34 +0200

    docs: Project structure and fixes

---
+++ docs/api/react-ui/intro.md
@@ -0,0 +1,26 @@
+---
+id: intro
+title: Introduction
+---
+
+@arke/react-ui offers a comprehensive suite of UI tools to help you ship new features faster
+
+## Usage
+
+First of all, you need to install the library:
+
+```sh
+yarn install @arke/react-ui
+npm install @arke/react-ui
+pnpm install @arke/react-ui
+```
+
+Then you're able to import the library and establish the connection with the database:
+
+```js
+import { Button } from '@arke/react-ui'
+
+return(
+    <Button>Click me</Button>
+)
+```
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,36 @@ import Layout from '@theme/Layout';
 
 export default function Index() {
     return (
-        <Layout title="Hello">
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height:'50vh',
-                    fontSize: '56px',
-                    textAlign:'center',
-                    fontWeight:'bold'
-                }}>
-                Optimise your project time.<br/>
-                OpenSource tools to build your apps.
-            </div>
-            <div style={{textAlign:'center'}}>
-                <a href="/docs/introduction">
-                    <button className="button-blue">
-                        Get Started
-                    </button>
-                </a>
-                <a href="https://github.com/arkemishub" target="_blank">
-                    <button>
-                        Github
-                    </button>
-                </a>
+        <Layout>
+            <div style={{
+                backgroundImage:'url(img/arke_background.png)',
+                backgroundSize: 'cover'
+            }}>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height:'50vh',
+                        fontSize: '56px',
+                        textAlign:'center',
+                        fontWeight:'bold'
+                    }}>
+                    Optimise your project time.<br/>
+                    OpenSource tools to build your apps.
+                </div>
+                <div style={{textAlign:'center'}}>
+                    <a href="/docs/introduction">
+                        <button className="button-blue">
+                            Get Started
+                        </button>
+                    </a>
+                    <a href="https://github.com/arkemishub" target="_blank">
+                        <button>
+                            Github
+                        </button>
+                    </a>
+                </div>
             </div>
         </Layout>
     );
