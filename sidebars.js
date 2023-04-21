453a5481ac54602786b6919e79628f8c1d8b1eb1
Erik <e.ferrari@arkemis.it>
2023-04-21 16:16:25 +0200

    fix: gitignore and sidebar

--- sidebars.js
+++ sidebars.js
@@ -2,14 +2,45 @@ module.exports = {
   docsSidebar: [
     "introduction",
     "architecture",
-    "getting-started/getting-started-local-installation",
+    {
+      type: 'category',
+      label: 'Think Arke',
+      items: [
+        'think-arke/glossary',
+        'think-arke/core-concepts',
+      ],
+    },{
+      type: 'category',
+      label: 'Tutorial',
+      items: [
+        'tutorial/installation',
+        'tutorial/console',
+        'tutorial/backend-starter',
+        'tutorial/create-first-arke',
+        'tutorial/customize-backend',
+      ],
+    },
     {
       type: 'category',
       label: 'Backend',
       items: [
-          'backend/elixir',
-          'backend/database',
-          'backend/authentication',
+        {
+          type: 'link',
+          label: 'Arke',
+          href: 'https://hexdocs.pm/arke/',
+        },{
+          type: 'link',
+          label: 'Arke Postgres',
+          href: 'https://hexdocs.pm/arke_postgres/',
+        },{
+          type: 'link',
+          label: 'Arke Auth',
+          href: 'https://hexdocs.pm/arke_auth/',
+        },{
+          type: 'link',
+          label: 'Arke Server',
+          href: 'https://hexdocs.pm/arke_server/',
+        },
         {
           type: 'category',
           label: 'Rest API',
