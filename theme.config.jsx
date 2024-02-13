4e76f79466fbbd906aeea677bfdeb481a956a770
Manolo Battista <manolo@arkemis.it>
2024-02-13 02:15:24 +0100

    docs: add favicon

--- theme.config.jsx
+++ theme.config.jsx
@@ -19,6 +19,7 @@ export default {
         link: 'https://github.com/arkemishub'
     },
     logo: <Image src="/arke.svg" alt="arke" width={110} height={60} />,
+    faviconGlyph: "/favicon/favicon.ico",
     useNextSeoProps() {
         return {
             titleTemplate: '%s – Arke'
@@ -29,6 +30,23 @@ export default {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content="Arke" />
             <meta property="og:description" content="The Arke Framework Docs" />
+            <link
+                rel="shortcut icon"
+                type="image/x-icon"
+                href={"/favicon/favicon.ico"}
+            />
+            <link
+                rel="icon"
+                type="image/x-icon"
+                sizes="16x16"
+                href={"/favicon/favicon-16x16.png"}
+            />
+            <link
+                rel="icon"
+                type="image/x-icon"
+                sizes="32x32"
+                href={"/favicon/favicon-32x32.png"}
+            />
         </>
     ),
     darkMode: false,
