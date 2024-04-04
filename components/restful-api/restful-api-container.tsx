fe9d901d65e0210ffc0224b2dfb1694e7c76c2ec
Manolo Battista <manolo@arkemis.it>
2024-04-04 19:18:23 +0200

    docs: restful api group

--- components/restful-api/restful-api-container.tsx
+++ components/restful-api/restful-api-container.tsx
@@ -10,10 +10,10 @@ export function RestfulApiContainer(props: RestfulApiContainerProps){
     const {children} = props;
     return(children &&
         <div className="mt-4 grid grid-cols-12 gap-6">
-            <div className="col-span-12 md:col-span-6">
+            <div className="col-span-12 lg:col-span-6">
                 {children[0]}
             </div>
-            <div className="col-span-12 md:col-span-6">
+            <div className="col-span-12 lg:col-span-6">
                 <div className="sticky top-[40px]">
                 {children[1]}
                 </div>
