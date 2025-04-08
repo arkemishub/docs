c2b4ecfe775f78bf4f75b3940e6f44247732630e
Manolo Battista <manolo@arkemis.it>
2025-04-08 18:01:19 +0200

    feat: add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,35 @@
 import "../global.css";
-import { NextComponentType } from "next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Toaster } from "@/components/ui/toaster";
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({
-  Component,
-  pageProps,
-}: {
-  Component: NextComponentType;
-  pageProps: any;
-}) {
+export default function App({ Component, pageProps }: AppProps) {
+  const gtmId = "GTM-TH7N73VR";
   return (
     <>
+      {/* Google Tag Manager */}
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');
+            `,
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
       <Toaster />
     </>
   );
